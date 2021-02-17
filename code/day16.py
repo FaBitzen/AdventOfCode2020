9d54b71d6079b778e840ea8b9e9f9a07b87995dc
Fabian <fabitzen@gmail.com>
2021-02-17 22:26:01 +0100

    completed Day 16 (close #8)

--- code/day16.py
+++ code/day16.py
@@ -1,76 +1,98 @@
+import regex
+
 with open("../input/day16.txt") as f:
-    rules, myticket, othertickets = f.read().split('\n\n')
+    raw_rules, myticket, othertickets = f.read().split('\n\n')
 
-_, myticket = myticket.split('\n')
-_, *othertickets, _ = othertickets.split('\n')
 
-temp = rules.split('\n')
+def ticket_to_int_list(ticket: str):
+    res = []
+    for val in ticket.split(','):
+        res.append(int(val))
+    return res
+
+
+myticket = ticket_to_int_list(myticket.split(':\n')[1])
+
+othertickets = othertickets.split(':\n')[1].split('\n')[:-1]
+othertickets = list(map(ticket_to_int_list,  othertickets))
+
 rules = dict()
-for rule in temp:
-    pos, ran = rule.split(": ")
-    rules[pos] = set()
-    ran1, ran2 = ran.split(" or ")
-    op1, op2 = ran1.split('-')
-    for i in range(int(op1), int(op2) + 1):
-        rules[pos].add(i)
-    op1, op2 = ran2.split('-')
-    for i in range(int(op1), int(op2) + 1):
-        rules[pos].add(i)
+pattern = "(.*?): ([\d]+)-([\d]+) or ([\d]+)-([\d]+)"
+for rule in raw_rules.split('\n'):
+    match = regex.match(pattern, rule)
+    values = set(range(int(match[2]), int(match[3]) + 1))
+    values = values.union(set(range(int(match[4]), int(match[5]) + 1)))
+    rules[match[1]] = values
 
-vals = set()
-for i in rules.values():
-    for j in i:
-        vals.add(j)
-
-invalid = []
-notright = set()
-for n, i in enumerate(othertickets):
-    for j in i.split(','):
-        if int(j) not in vals:
-            notright.add(n)
-            invalid.append(int(j))
-print(sum(invalid))
-
-notright = list(notright)
-notright.sort(reverse=True)
-for i in notright:
-    othertickets.pop(i)
-
-fields = dict()
-for i, field in enumerate(rules.keys()):
-    fields[i] = field
-
-otherrows = dict()
-for i in range(len(othertickets[0].split(','))):
-    otherrows[i] = set()
-    for j in othertickets:
-        otherrows[i].add(int(j.split(',')[i]))
+possible_values = set()
+for value in rules.values():
+    possible_values = possible_values.union(value)
 
 
-def addvalue(check, value, n):
-    if isvalid(check, value):
-        if len(check) > 8:
-            print(check + (value,))
-        if len(check) < n - 1:
-            for i in range(n):
-                yield from addvalue(check + (value,), i, n)
+def validate_tickets(tickets: list[list[int]]) -> list[list[int]]:
+    res = []
+    for ticket in tickets:
+        for para in ticket:
+            if para not in possible_values:
+                break
         else:
-            yield check + (value,)
+            res.append(ticket)
+    return res
+
+
+def part1():
+    acc = 0
+    for ticket in othertickets:
+        for para in ticket:
+            if para not in possible_values:
+                acc += para
+    return acc
+
+
+valid_tickets = validate_tickets(othertickets)
+valid_tickets.append(myticket)
+categories = list(zip(*valid_tickets))
+
+
+def isvalid(check, val):
+    if val in check:
+        return False
+    return rules[val].issuperset(categories[len(check)])
+
+
+def addvalue(check, val, n):
+    if isvalid(check, val):
+        if len(check) < n - 1:
+            print(check + (val,))
+            for i in rules.keys():
+                yield from addvalue(check + (val,), i, n)
+        else:
+            yield check + (val,)
     return False
 
 
-def isvalid(check,  value) -> bool:
-    if value in check:
-        return False
-    if otherrows[len(check)].issubset(rules[fields[value]]):
-        return False
-    return True
+def get_order() -> list:
+    res = [None for _ in range(len(rules))]
+    possibilyties = dict([(i, []) for i in rules.keys()])
+    for key, val in rules.items():
+        for i, category in enumerate(categories):
+            if val.issuperset(category):
+                possibilyties[key].append(i)
+    possibilyties = list(possibilyties.items())
+    possibilyties.sort(key=lambda t: len(t[1]))
+    for key, val in possibilyties:
+        for i in val:
+            if not res[i]:
+                res[i] = key
+    return res
 
 
-def queens(n: int):
-    for i in range(n):
-        yield from addvalue((), i, n)
+def part2():
+    acc = 1
+    for val, name in zip(myticket, get_order()):
+        if name.startswith("departure"):
+            acc *= val
+    return acc
 
 
-result = queens(len(fields))
-print(list(result))
+print(part1(), part2())
